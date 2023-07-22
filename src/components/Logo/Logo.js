436532bef76ed1cf2f5f2ba6ffeddc3ebb940eac
Papon Mateevorakulkij <furby153@gmail.com>
2023-07-22 15:05:13 +0700

    Add Logo

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,13 +1,21 @@
 import React from "react";
 import Tilt from 'react-parallax-tilt';
+import './Logo.css';
+import brain from './brain-ai.png';
 
 const Logo = () => {
     return (
         <div className='ma4 mt0'>
-            <Tilt>
-            <div style={{ height: '150px', width: '150px', backgroundColor: 'yellow' }}>
-                <h1>React Parallax Tilt 👀</h1>
-            </div>
+            <Tilt
+                className="parallax-effect-glare-scale"
+                perspective={500}
+                glareEnable={true}
+                glareMaxOpacity={0.7}
+                scale={1.02}
+            >
+                <div className="inner-element">
+                <div><img style={{padding: '5px'}}src={brain} alt='logo'/></div>
+                </div>
             </Tilt>
         </div>
     );
