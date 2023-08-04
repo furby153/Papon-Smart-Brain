ccc249a325b862065e5b108771b66eccc8b17440
Papon Mateevorakulkij <furby153@gmail.com>
2023-08-04 16:42:09 +0700

    edit ImageLinkForm.js

--- src/App.js
+++ src/App.js
@@ -1,21 +1,42 @@
 import './App.css';
+import React from 'react';
 import ParticlesBg from 'particles-bg'
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import Rank from './components/Rank/Rank';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 
-function App() {
-  return (
-    <div className="App">
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm />
-      {/* <FaceRecognition /> */}
-      <ParticlesBg color="#ffff00" num={100} type="cobweb" bg={true} />
-    </div>
-  );
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+    }
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  }
+
+  onButtonSubmit = () => {
+    console.log('click');
+  }
+  render() {
+    return (
+      <div className="App">
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm 
+          onInputChange={this.onInputChange} 
+          onButtonSubmit={this.onButtonSubmit}
+        />
+        {/* <FaceRecognition /> */}
+        <ParticlesBg color="#ffff00" num={100} type="cobweb" bg={true} />
+      </div>
+    );
+  }
+  
 }
 
 export default App;
