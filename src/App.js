d83f326ca6444040b6ad633f154456ee52e7b4d4
Papon Mateevorakulkij <furby153@gmail.com>
2023-08-05 18:40:23 +0700

    delete some comments on App.js

--- src/App.js
+++ src/App.js
@@ -6,21 +6,14 @@ import Logo from './components/Logo/Logo';
 import Rank from './components/Rank/Rank';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import FaceRecognition from './components/FaceRecognition/FaceRecognition';
-// import Clarifai from 'clarifai';
 
 const returnClarifaiRequestOptions = (imageUrl) => {
-    // In this section, we set the user authentication, user and app ID, model details, and the URL
-    // of the image we want as an input. Change these strings to run your own example.
-    //////////////////////////////////////////////////////////////////////////////////////////////////
-
     // Your PAT (Personal Access Token) can be found in the portal under Authentification
     const PAT = 'c5399a2074724d5e9e660023be3cd7d1';
     // Specify the correct user_id/app_id pairings
     // Since you're making inferences outside your app's scope
     const USER_ID = 'paponmat';       
     const APP_ID = 'Smart-Brain';
-    // Change these to whatever model and image URL you want to use
-    // const MODEL_ID = 'face-detection';
     // const MODEL_VERSION_ID = '6dc7e46bc9124c5c8824be4822abe105';    
     const IMAGE_URL = imageUrl;
 
@@ -72,12 +65,9 @@ class App extends React.Component {
 
   onButtonSubmit = async () => {
     this.setState({imageUrlState: this.state.input});
-    
+    // Change this to whatever model you want to use
     const MODEL_ID = 'face-detection'
 
-    // NOTE: MODEL_VERSION_ID is optional, you can also call prediction with the MODEL_ID only
-    // https://api.clarifai.com/v2/models/{YOUR_MODEL_ID}/outputs
-    // this will default to the latest version_id
     try {
       const response = await fetch("https://api.clarifai.com/v2/models/" + MODEL_ID + "/outputs", returnClarifaiRequestOptions(this.state.input));
       const result = await response.json();
