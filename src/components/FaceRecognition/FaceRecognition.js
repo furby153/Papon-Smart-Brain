62b7cdacdd48d6ffdd98556fa05cdf39b324cdb0
Papon Mateevorakulkij <furby153@gmail.com>
2023-08-05 18:17:07 +0700

    edit FaceRecognition

--- src/components/FaceRecognition/FaceRecognition.js
+++ src/components/FaceRecognition/FaceRecognition.js
@@ -3,12 +3,15 @@ import './FaceRecognition.css'
 
 const FaceRecognition = ({ imageUrl }) => {
     return (
-        <div className='center'>
-            <img
-                className='imageToShow'
-                src={imageUrl}
-                alt=''
-            /> 
+        <div className='center ma'>
+            <div className='absolute mt2'>
+                <img
+                    className='imageToShow'
+                    src={imageUrl}
+                    alt=''
+                /> 
+            </div>
+            
         </div>
     );
 }
